32f6bb8896a4707ddff7546fcb23898e7e38dae1
Dan Draper <dan@cipherstash.com>
2022-08-18 11:42:47 +1000

    Updated to use cipherstash-client 0.4.0-beta.0

--- src/schema.rs
+++ src/schema.rs
@@ -19,13 +19,13 @@ pub fn collection_schema() -> CollectionSchema {
         indexes: collection! {
             "exactTitle" => MappingWithMeta {
                 mapping: Mapping::Exact { field: "title".into() },
-                index_id: *Uuid::parse_str("65e381f5-18dc-41af-8a89-d0641b09accc").unwrap().as_bytes(),
+                index_id: Uuid::parse_str("65e381f5-18dc-41af-8a89-d0641b09accc").unwrap(),
                 prf_key: [0;16],
                 prp_key: [0;16]
             },
             "runningTime" => MappingWithMeta {
                 mapping: Mapping::Range { field: "runningTime".into() },
-                index_id: *Uuid::parse_str("f83f2cf4-4fc9-4147-9c39-7b99eafa74df").unwrap().as_bytes(),
+                index_id: Uuid::parse_str("f83f2cf4-4fc9-4147-9c39-7b99eafa74df").unwrap(),
                 prf_key: [0;16],
                 prp_key: [0;16]
             }
